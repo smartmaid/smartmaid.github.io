9fa64941f003a720ad4c36000049195c04660eef
smartmaid <smartmaid.cemerlang@gmail.com>
2020-05-23 22:46:26 +0700

    edit data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,11 @@
-var map, infoWindow;
+var map;
 
-function createMap() {
-  var options = {
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: -6.91836432,
       lng: 107.61358649
     },
-    zoom: 10
-  };
-
-  map = new google.maps.Map(document.getElementById('map'), options);
-  infoWindow = new google.maps.InfoWindow;
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (p) {
-      var position = {
-        lat: p.coords.latitude,
-        lng: p.coords.longitude
-      };
-
-      infoWindow.setPosition(position);
-      infoWindow.setContent('Your location!');
-      infoWindow.open(map);
-      map.setCenter(position);
-    }, function () {
-      handleLocationError('Geolocation service failed', map.getCenter());
-    });
-  } else {
-    handleLocationError('No geolocation available.', map.getCenter());
-  }
-}
-
-function handleLocationError(content, position) {
-  infoWindow.setPosition(position);
-  infoWindow.setContent(content);
-  infoWindow.open(map);
+    zoom: 8
+  });
 }
